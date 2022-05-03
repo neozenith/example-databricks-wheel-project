9ae3ad0bf70b8f0d007d18d6d97adaf7cafe0eff
Josh Peak <josh.peak@86400.com.au>
2022-05-03 23:36:39 +1000

    Add helper commands for wheel names

--- tasks.py
+++ tasks.py
@@ -1,3 +1,16 @@
 # Third Party
 from invoke_common_tasks import *  # noqa
 from invoke_databricks_wheel_tasks import *  # noqa
+from invoke_databricks_wheel_tasks.utils.poetry import poetry_wheelname
+from invoke_databricks_wheel_tasks.utils.databricks import default_dbfs_wheel_path
+from invoke import task
+
+@task
+def wheel_name(c):
+    """Name of the wheel."""
+    print(poetry_wheelname())
+
+@task
+def dbfs_wheel(c, branch=None):
+    """DBFS wheel path."""
+    print(default_dbfs_wheel_path(branch))
